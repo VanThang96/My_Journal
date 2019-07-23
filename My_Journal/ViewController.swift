093434650fddfff0589decab3bb2c8d75bbdb2d0
Nguyen Van Thang <nguyenvanthang@nguyens-macbook-pro.local>
2019-07-23 14:40:38 +0700

    Initial Commit

---
+++ My_Journal/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  My_Journal
+//
+//  Created by Nguyen Van Thang on 7/23/19.
+//  Copyright © 2019 Nguyen Van Thang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
